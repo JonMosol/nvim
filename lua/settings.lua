497788bad8e155be3ed92e56c7eef8437b390b22
Jonathan Mosol <jonathan_mosol@baxter.com>
2026-01-26 15:06:19 -0800

    Fixing some UI defaults
    Fixing some UI defaults

--- lua/settings.lua
+++ lua/settings.lua
@@ -5,6 +5,7 @@ vim.g.maplocalleader = "\\"
 -- Editor settings
 vim.opt.scrolloff = 20         -- Keep 8 lines above/below cursor
 vim.opt.sidescrolloff = 8     -- Keep 8 columns left/right of cursor
+vim.opt.wrap = false          -- Disable word wrap
 
 -- Line numbers
 vim.opt.number = true         -- Show absolute line numbers
